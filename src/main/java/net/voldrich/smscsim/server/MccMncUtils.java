9d03386f42157296a61cc2ece2c8cc7be2566592
micoenaogongob <mico.gongob@synacy.com>
2020-02-14 14:15:03 +0800

    remove prefix in vars in MccMncUtils.java -> MOBILE_

--- src/main/java/net/voldrich/smscsim/server/MccMncUtils.java
+++ src/main/java/net/voldrich/smscsim/server/MccMncUtils.java
@@ -4,10 +4,10 @@ public class MccMncUtils {
 
   // TODO: Get from config/env
   private final static String GATEWAY_SERVICE_TLV_TAG = "1400";
-  private final static String MOBILE_COUNTRY_CODE = "63";
-  private final static String MOBILE_NETWORK_CODE = "0998";
+  private final static String COUNTRY_CODE = "63";
+  private final static String NETWORK_CODE = "0998";
 
   public final static short TAG_MCC_MNC = Short.parseShort(GATEWAY_SERVICE_TLV_TAG.trim(), 16);
-  public final static String SIMULATOR_MCC_MNC_VALUE = MOBILE_COUNTRY_CODE + MOBILE_NETWORK_CODE;
+  public final static String SIMULATOR_MCC_MNC_VALUE = COUNTRY_CODE + NETWORK_CODE;
 
 }
