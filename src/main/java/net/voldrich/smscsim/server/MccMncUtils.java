b5f3932fdab8c4f5b887a083d2da6d1cbef8d62d
micoenaogongob <mico.gongob@synacy.com>
2020-02-14 17:20:31 +0800

    refactor hex decoding in MccMncUtils

--- src/main/java/net/voldrich/smscsim/server/MccMncUtils.java
+++ src/main/java/net/voldrich/smscsim/server/MccMncUtils.java
@@ -3,11 +3,11 @@ package net.voldrich.smscsim.server;
 public class MccMncUtils {
 
   // TODO: Get from config/env
-  private final static String GATEWAY_SERVICE_TLV_TAG = "1400";
+  private final static String GATEWAY_SERVICE_TLV_TAG = "0x1400";
   private final static String COUNTRY_CODE = "123";
   private final static String NETWORK_CODE = "546";
 
-  public final static short TAG_MCC_MNC = Short.parseShort(GATEWAY_SERVICE_TLV_TAG.trim(), 16);
+  public final static short TAG_MCC_MNC = Short.decode(GATEWAY_SERVICE_TLV_TAG.trim());
   public final static String SIMULATOR_MCC_MNC_VALUE = COUNTRY_CODE + NETWORK_CODE;
 
 }
