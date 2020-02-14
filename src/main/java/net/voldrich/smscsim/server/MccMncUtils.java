6b03760a9abf1c7ce86fea7d311b7cc943648d2e
micoenaogongob <mico.gongob@synacy.com>
2020-02-14 15:50:34 +0800

    change Mobile Code values in MccMncUtils

--- src/main/java/net/voldrich/smscsim/server/MccMncUtils.java
+++ src/main/java/net/voldrich/smscsim/server/MccMncUtils.java
@@ -4,8 +4,8 @@ public class MccMncUtils {
 
   // TODO: Get from config/env
   private final static String GATEWAY_SERVICE_TLV_TAG = "1400";
-  private final static String COUNTRY_CODE = "63";
-  private final static String NETWORK_CODE = "0998";
+  private final static String COUNTRY_CODE = "123";
+  private final static String NETWORK_CODE = "546";
 
   public final static short TAG_MCC_MNC = Short.parseShort(GATEWAY_SERVICE_TLV_TAG.trim(), 16);
   public final static String SIMULATOR_MCC_MNC_VALUE = COUNTRY_CODE + NETWORK_CODE;
