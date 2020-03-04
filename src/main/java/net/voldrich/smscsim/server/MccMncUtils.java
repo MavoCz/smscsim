0f44326a092dd113808338583d665800c1b7a7db
micoenaogongob <mico.gongob@synacy.com>
2020-03-04 15:12:45 +0800

    add default values for environment variables fetched in MccMncUtils.java

--- src/main/java/net/voldrich/smscsim/server/MccMncUtils.java
+++ src/main/java/net/voldrich/smscsim/server/MccMncUtils.java
@@ -14,16 +14,18 @@ public class MccMncUtils {
   public static Tlv SIMULATOR_MCC_MNC_TLV;
 
   public MccMncUtils(Properties properties) {
-    String tlvTag = getNullSafeProperty(properties,"TLV_TAG");
-    String countryCode = getNullSafeProperty(properties,"MOBILE_COUNTRY_CODE");
-    String networkCode = getNullSafeProperty(properties,"MOBILE_NETWORK_CODE");
+    String tlvTag = getNullSafeProperty(properties, "TLV_TAG", "0x1400");
+    String countryCode = getNullSafeProperty(properties, "MOBILE_COUNTRY_CODE", "123");
+    String networkCode = getNullSafeProperty(properties, "MOBILE_NETWORK_CODE", "456");
 
-    LOGGER.info("Tlv tag: {}, country code: {}, network code: {}", tlvTag, countryCode, networkCode);
+    LOGGER
+        .info("Tlv tag: {}, country code: {}, network code: {}", tlvTag, countryCode, networkCode);
 
     SIMULATOR_MCC_MNC_TLV = createSimulatorMccMncTlv(tlvTag, countryCode, networkCode);
   }
 
-  private static Tlv createSimulatorMccMncTlv(String tlvTag, String countryCode, String networkCode) {
+  private static Tlv createSimulatorMccMncTlv(String tlvTag, String countryCode,
+      String networkCode) {
     final short tlvTagToShort = Short.decode(tlvTag.trim());
     final String simulatorMccMncValue = countryCode + networkCode;
 
@@ -34,8 +36,8 @@ public class MccMncUtils {
     return SIMULATOR_MCC_MNC_TLV;
   }
 
-  private String getNullSafeProperty(Properties properties, String key) {
-    return null == properties.getProperty(key) ? "" : properties.getProperty(key);
+  private String getNullSafeProperty(Properties properties, String key, String defaultVal) {
+    return null != properties.getProperty(key) ? properties.getProperty(key) : defaultVal;
   }
 
 }
